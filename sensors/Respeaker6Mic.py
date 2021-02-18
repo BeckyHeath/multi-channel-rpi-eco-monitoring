ca8e66d3f6d7d39232aa5368bee8d3c024442cb7
BeckyHeath <r.heath18@imperial.ac.uk>
2021-02-18 13:28:26 +0000

    fix typo in FLAC line

--- sensors/Respeaker6Mic.py
+++ sensors/Respeaker6Mic.py
@@ -111,7 +111,7 @@ class Respeaker6Mic(SensorBase):
             ofile = os.path.join(self.upload_dir, self.current_file) + '.flac'
 
             logging.info('\n{} - Starting compression\n'.format(self.current_file))
-            cmd = ('ffmpeg -I {} -:a flac {} >/dev/null 2>&1') 
+            cmd = ('ffmpeg -I {} -c:a flac {} >/dev/null 2>&1') 
             subprocess.call(cmd.format(wfile, ofile), shell=True)
             logging.info('\n{} - Finished compression\n'.format(self.current_file))
         else:
