1fa6e30672e42b3a936bbd929ee49ce69c447a8d
JamesSkinna <james.skinner19@imperial.ac.uk>
2023-02-16 22:46:43 +0000

    Added code for button, SD card & battery shutdown

--- recorder_startup_script.sh
+++ recorder_startup_script.sh
@@ -2,9 +2,6 @@
 
 printf '#############################################\nStart of ecosystem monitoring startup script\n############################################\n'
 
-# just as a back up schedule a reboot for 24 hours (in case something goes wrong before scheduling the 2am reboot)
-(sudo shutdown -r +1440) &
-
 # One off expanding of filesystem to fill SD card
 if [ ! -f fs_expanded ]; then
   sudo touch fs_expanded
@@ -17,7 +14,7 @@ sudo service udev stop
 sudo service udev start
 
 tries=0
-max_tries=50
+max_tries=10
 while true; do
 	timeout 2s wget -q --spider http://google.com
 	if [ $? -eq 0 ]; then
@@ -48,16 +45,16 @@ sudo bash ./bash_update_time.sh
 eval $(ssh-agent -s)
 
 # Pull latest code from repo
-last_sha=$(git rev-parse HEAD)
-git fetch origin
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-git reset --hard origin/$branch
-printf 'Pulled from github\n'
-now_sha=$(git rev-parse HEAD)
+#last_sha=$(git rev-parse HEAD)
+#git fetch origin
+#branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+#git reset --hard origin/$branch
+#printf 'Pulled from github\n'
+#now_sha=$(git rev-parse HEAD)
 
 # Check if this file has changed - reboot if so
-changed_files="$(git diff-tree -r --name-only --no-commit-id $last_sha $now_sha)"
-echo "$changed_files" | grep --quiet "recorder_startup_script" && sudo reboot
+#changed_files="$(git diff-tree -r --name-only --no-commit-id $last_sha $now_sha)"
+#echo "$changed_files" | grep --quiet "recorder_startup_script" && sudo reboot
 
 # Add in current date and time to log files
 currentDate=$(date +"%Y-%m-%d_%H.%M")
